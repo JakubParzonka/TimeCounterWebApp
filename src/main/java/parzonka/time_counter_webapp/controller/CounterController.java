1de474c7c45dc76311b2540c07211d442ba095f0
JakubParzonka <jakub.parzonka@outlook.com>
2018-03-24 14:16:20 +0100

    Passing value from form to controller.

--- src/main/java/parzonka/time_counter_webapp/controller/CounterController.java
+++ src/main/java/parzonka/time_counter_webapp/controller/CounterController.java
@@ -1,25 +1,40 @@
 package parzonka.time_counter_webapp.controller;
 
+import javafx.application.Application;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import parzonka.time_counter_webapp.model.ParameterS;
+
+import java.lang.reflect.Parameter;
 
 @Controller
 public class CounterController {
 
     @RequestMapping(value = "/counter", method = RequestMethod.GET)
-    public String getMainCounterPage(Model model){
+    public String getMainCounterPage(Model model) {
         return "counter";
     }
 
     @RequestMapping(value = "paramTi", method = RequestMethod.GET)
-    public String showTiParametersPage(Model model){
+    public String showTiParametersPage(Model model) {
         return "paramTi";
     }
+
     @RequestMapping(value = "paramF", method = RequestMethod.GET)
-    public String showFreqParametersPage(Model model){
+    public String showFreqParametersPage(Model model) {
         return "paramF";
+    }
+
+    @RequestMapping(value = "saveTiParams", method = RequestMethod.POST)
+    public @ResponseBody
+    String saveTiParams(@RequestBody ParameterS paramS) {
+
+        System.out.println(paramS.toString());
+        return "";
     }
 
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
 
-    $("#confirmSconfig").click(function () {
-        console.log("startInput => " + $('input[name=startInput]:checked', '#startInputForm').val());
-        console.log("startSlote => " + $('input[name=startSlote]:checked', '#startSloteForm').val());
-        console.log("stopInput => " + $('input[name=stopInput]:checked', '#stopInputForm').val());
-        console.log("stopSlote => " + $('input[name=stopSlote]:checked', '#stopSloteForm').val());
-        console.log("clockInteral => " + $('input[name=clockInteral]:checked', '#clockInteralForm').val());
-        console.log("calibrated => " + $('input[name=calibrated]:checked', '#calibratedForm').val());
-        console.log("enEnd => " + $('input[name=enEnd]:checked', '#enEndForm').val());
-    });
-
-    // clearing modes page on start
-    $('#content').empty();
-
     $("#modeFrom").on('change', function () {
         var mode = $('input[name=mode]:checked', '#modeFrom').val();
         var contentDiv = $('#content');
@@ -25,10 +12,6 @@ $(document).ready(function () {
         }
     });
 
-    $("p").click(function () {
-        var htmlString = $(this).html();
-        $(this).text(htmlString);
-    });
 });
 
 
