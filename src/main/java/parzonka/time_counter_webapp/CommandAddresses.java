04eec27cc2a10ac048a5b4b765e7305e38082abe
JakubParzonka <jakub.parzonka@outlook.com>
2018-03-26 17:27:36 +0200

    Creating addresses for counter commands

--- src/main/java/parzonka/time_counter_webapp/CommandAddresses.java
+++ src/main/java/parzonka/time_counter_webapp/CommandAddresses.java
@@ -6,82 +6,88 @@ import org.apache.log4j.Logger;
 import java.util.BitSet;
 
 public class CommandAddresses {
+
     private static Logger logger = LogManager.getLogger(TimeCounterWebAppApplication.class);
 
-    public static BitSet getWriteOfEnBitSetAddres() {
-        // $03
+    /**
+     * @return
+     */
+    public static BitSet getWriteResetAndACalibrationAddr() {
+        // $00
         BitSet bs = new BitSet();
-        bs.set(0,2, true);
-        bs.set(8,true);
-        bs.set(9,true);
-        for (int i = 10; i < 40; i++) {
-            bs.set(i, false);
-        }
-//        bs.set(39,true);
-        logger.info("Write of EN register => " + bs.toString());
+        bs.set(0, 9, false);
+        logger.info("RESET register => " + bs.toString());
         return bs;
     }
 
-    public static BitSet getReadOfEnBitSetAddres() {
-        // $F3
-        BitSet bs = new BitSet();
-        bs.set(0,2, true);
-        bs.set(4, 9, true);
-        for (int i = 10; i < 40; i++) {
-//            if (i % 2 == 0)
-            bs.set(i, false);
-        }
-
-        logger.info("Read of EN register => " + bs.toString());
-        return bs;
-    }
-
-    public static BitSet getRESET_BitSetAddres() {
-        // 01000011
-        BitSet bs = new BitSet();
-        bs.set(0, 1, true);
-        bs.set(2, 5, false);
-        bs.set(6, true);
-        bs.set(7, false);
-        logger.info("CA => RESET - " + bs.toString());
-        return bs;
-    }
-
-
-    public static BitSet getSET_TRIG_BitSetAddres() {
-        // 01000100
-        BitSet bs = new BitSet();
-        bs.set(0, 2, false);
-        bs.set(3, true);
-        bs.set(4, 6, false);
-        bs.set(6, true);
-        bs.set(7, false);
-        logger.info("CA => SET_TRIG - " + bs.toString());
-        return bs;
-    }
-
-    public static BitSet getTIRG_DIV_BitSetAddres() {
-        // 01000101
+    /**
+     * @return
+     */
+    public static BitSet getWriteMeasurementAddr() {
+        // $01
         BitSet bs = new BitSet();
         bs.set(0, true);
-        bs.set(1, false);
-        bs.set(2, true);
-        bs.set(3, 5, false);
-        bs.set(6, true);
-        bs.set(7, false);
-        logger.info("CA => TRIG_DIV - " + bs.toString());
+        logger.info("MEAS register => " + bs.toString());
         return bs;
     }
 
-    public static BitSet getSYNTH_N_BitSetAddres() {
-        // 0F
+    /**
+     * @return
+     */
+    public static BitSet getWriteSregAddr() {
+        // $02
         BitSet bs = new BitSet();
-        bs.set(0, 1, true);
-        bs.set(2, 5, false);
-        bs.set(6, true);
-        bs.set(7, false);
-        logger.info("CA => SYNTH_N - " + bs.toString());
+        bs.set(1, true);
+        logger.info("SET_S register => " + bs.toString());
         return bs;
     }
+
+    /**
+     * @return
+     */
+    public static BitSet getWriteEnRegAddr() {
+        // $03
+        BitSet bs = new BitSet();
+        bs.set(0, 2, true);
+        logger.info("SET_EN register => " + bs.toString());
+        return bs;
+    }
+
+    /**
+     * @return
+     */
+    public static BitSet getReadDataAddr() {
+        // $F0
+        BitSet bs = new BitSet();
+        bs.set(4, 9, true);
+        logger.info("RD_F_DATA register => " + bs.toString());
+        return bs;
+    }
+
+    /**
+     * @return
+     */
+    public static BitSet getReadSregAddr() {
+        // $F2
+        BitSet bs = new BitSet();
+        bs.set(1, true);
+        bs.set(4, 9, true);
+        logger.info("RD_S register => " + bs.toString());
+        return bs;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static BitSet getReadEnRegAddr() {
+        // $F3
+        BitSet bs = new BitSet();
+        bs.set(0, 2, true);
+        bs.set(4, 9, true);
+        logger.info("RD_EN register => " + bs.toString());
+        return bs;
+    }
+
 }
 
