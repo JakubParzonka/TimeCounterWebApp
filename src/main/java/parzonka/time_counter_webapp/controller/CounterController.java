f0ca928344bb8b89dcc3634ec4d43a800396b446
JakubParzonka <jakub.parzonka@outlook.com>
2018-03-30 14:30:01 +0200

    Refactor v3

--- src/main/java/parzonka/time_counter_webapp/controller/CounterController.java
+++ src/main/java/parzonka/time_counter_webapp/controller/CounterController.java
@@ -9,11 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import parzonka.time_counter_webapp.CommandAddresses;
 import parzonka.time_counter_webapp.model.MessageWrapper;
-import parzonka.time_counter_webapp.model.ParameterCtrl;
-import parzonka.time_counter_webapp.model.ParameterS;
 import parzonka.time_counter_webapp.model.ParameterWrapper;
 import parzonka.time_counter_webapp.service.CounterService;
-import parzonka.time_counter_webapp.utils.CommandUtill;
 
 @Controller
 public class CounterController {
@@ -40,18 +37,7 @@ public class CounterController {
     public @ResponseBody
     String saveParams(@RequestBody ParameterWrapper parameterWrapper) {
         if (parameterWrapper != null) {
-            System.out.println(parameterWrapper.toString());
-
-            ParameterS parameterS = new ParameterS(parameterWrapper.getStartInput(), parameterWrapper.getStartSlote(),
-                    parameterWrapper.getStopInput(), parameterWrapper.getStopSlote(), parameterWrapper.getClockInternal());
-            MessageWrapper sWrapper = new MessageWrapper(CommandUtill.mapSregCommand(
-                    CommandAddresses.getWriteSregAddr(), parameterS).toByteArray());
-            counterService.sendData(sWrapper);
-
-            ParameterCtrl parameterCtrl = new ParameterCtrl(parameterWrapper.getTimeRange(), parameterWrapper.getEntrance());
-            MessageWrapper ctrlWrapper = new MessageWrapper(CommandUtill.mapCtrlCommand(
-                    CommandAddresses.getWriteCtrlAddr(), parameterCtrl).toByteArray());
-            counterService.sendData(ctrlWrapper);
+            counterService.saveStartingParams(parameterWrapper);
             return "Success!";
         } else {
             return "Failure saving params";
@@ -61,13 +47,8 @@ public class CounterController {
     @RequestMapping(value = "/startMeasurement", method = RequestMethod.POST)
     public @ResponseBody
     String startMeasurement() {
-        MessageWrapper startMeasurment = new MessageWrapper(CommandAddresses.getWriteMeasurementAddr().toByteArray());
-        counterService.sendData(startMeasurment);
-
-        MessageWrapper startReceingData = new MessageWrapper(CommandAddresses.getReadDataAddr().toByteArray());
-        counterService.sendData(startReceingData);
-        
-        return "3,11512312 ms";
+        counterService.startMeasrumentProcess();
+        return counterService.readData();
     }
 
     @RequestMapping(value = "readSregParams", method = RequestMethod.POST)
--- src/main/java/parzonka/time_counter_webapp/service/CounterService.java
+++ src/main/java/parzonka/time_counter_webapp/service/CounterService.java
@@ -8,6 +8,10 @@ import parzonka.time_counter_webapp.CommandAddresses;
 import parzonka.time_counter_webapp.Connection;
 import parzonka.time_counter_webapp.TimeCounterWebAppApplication;
 import parzonka.time_counter_webapp.model.MessageWrapper;
+import parzonka.time_counter_webapp.model.ParameterCtrl;
+import parzonka.time_counter_webapp.model.ParameterS;
+import parzonka.time_counter_webapp.model.ParameterWrapper;
+import parzonka.time_counter_webapp.utils.CommandUtill;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -46,6 +50,7 @@ public class CounterService {
      */
     public void sendData(MessageWrapper messageWrapper) {
         try {
+            // TODO stworzyć nowy exception jakiś dla npe
             DataOutputStream output = new DataOutputStream(connection.getOutputDataStream());
             output.write(messageWrapper.getMessage(), 0, messageWrapper.getLength());
             output.flush();
@@ -55,25 +60,62 @@ public class CounterService {
         }
     }
 
-    public String readData(){
+    /**
+     * Read incoming data from socket
+     *
+     * @return String representation of result
+     */
+    public String readData() {
         ByteArrayOutputStream bufferStream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1460];
         try {
             DataInputStream inputData = new DataInputStream(connection.getInputDataStream());
-            while (inputData.available() > 0) {
+            if (inputData.available() > 0) {
                 int bytesRead = inputData.read(buffer);
                 bufferStream.write(buffer, 0, bytesRead);
                 if (inputData.available() == 0) {
                     logger.warn("Input data available is 0");
                 }
-            } /*else {
-                logger.error("Input data is not availble");
-            }*/
+            } else {
+                logger.error("Input data is nolt availble");
+            }
         } catch (IOException e) {
             logger.error("Error while reading from socket", e);
         }
         return Arrays.toString(buffer);
     }
 
+    /**
+     * Send starting parameters to counter
+     *
+     * @param parameterWrapper
+     */
+    public void saveStartingParams(ParameterWrapper parameterWrapper) {
 
+        System.out.println(parameterWrapper.toString());
+
+        ParameterS parameterS = new ParameterS(parameterWrapper.getStartInput(), parameterWrapper.getStartSlote(),
+                parameterWrapper.getStopInput(), parameterWrapper.getStopSlote(), parameterWrapper.getClockInternal());
+        MessageWrapper sWrapper = new MessageWrapper(CommandUtill.mapSregCommand(
+                CommandAddresses.getWriteSregAddr(), parameterS).toByteArray());
+            sendData(sWrapper);
+
+        ParameterCtrl parameterCtrl = new ParameterCtrl(parameterWrapper.getTimeRange(), parameterWrapper.getEntrance());
+        MessageWrapper ctrlWrapper = new MessageWrapper(CommandUtill.mapCtrlCommand(
+                CommandAddresses.getWriteCtrlAddr(), parameterCtrl).toByteArray());
+        sendData(ctrlWrapper);
+    }
+
+    /**
+     * Starting measurement process
+     */
+    public void startMeasrumentProcess() {
+
+        MessageWrapper startMeasurment = new MessageWrapper(CommandAddresses.getWriteMeasurementAddr().toByteArray());
+        sendData(startMeasurment);
+
+        MessageWrapper startReceingData = new MessageWrapper(CommandAddresses.getReadDataAddr().toByteArray());
+        sendData(startReceingData);
+
+    }
 }
